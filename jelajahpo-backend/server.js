dfd65d53ec82819028931be4db1bc3709a6bcc64
Meowmiaww <rengganisarin@gmail.com>
2026-08-24 14:13:35 +0700

    server.js
    membuat route get wisata dan kategori

--- jelajahpo-backend/server.js
+++ jelajahpo-backend/server.js
@@ -1,6 +1,38 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'jelajahpo_db'
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Gagal konek ke database', err);
+    } else {
+        console.log('Berhasil konek ke database JelajahPo');
+    }
+});
+
+app.get('/wisata', (req, res) => {
+    const sql = 'SELECT * FROM wisata';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
+});
+
+app.get('/kategori', (req, res) => {
+    const sql = 'SELECT * FROM kategori';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
+});
 
 app.use(express.json());
 
